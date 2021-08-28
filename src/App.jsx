f97fd04922b661d12ffd095801ed276bd7bcfe34
Andres Yanquen <andres.yanquen@hotmail.com>
2021-08-28 17:27:20 -0500

    appbar modificado

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,49 @@
-import React from 'react';
-import {Button} from '@material-ui/core'
-import {makeStyles} from '@material-ui/core/styles';
+import React, { useState } from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import InputBase from '@material-ui/core/InputBase';
+import { AccountCircle } from '@material-ui/icons';
+import { useStyles } from './styles/Appbar/styles'
+import { Typography } from '@material-ui/core';
 
-const useStyle = makeStyles({
-  boton_1:{
-    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-    border: 0,
-    borderRadius: 3,
-    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-    color: 'white',
-    height: 48,
-    padding: '0 30px',
-  }
-})
 function App() {
 
-  const clases = useStyle()
-  return (
-    <div >
-      <Button
-        className={clases.boton_1}
-      > Mi boton personalizado </Button>
+  const classes = useStyles();
 
+  return (
+    <div className={classes.grow}>
+      <AppBar position="static">
+        <Toolbar>
+          <div className={classes.title}>
+          <h3>Buscar</h3>
+          <div className={classes.search}>
+            <InputBase
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </div>
+          </div>
+          {/* <div className={classes.grow} /> */}
+          <div className={classes.sectionDesktop}>
+            {/* User button */}
+            <IconButton
+              edge="end"
+              aria-label="account of current user"
+              aria-controls={""}
+              aria-haspopup="true"
+              onClick={() => console.log("test")}
+              color="inherit"
+            >
+              <AccountCircle />
+            </IconButton>
+          </div>
+        </Toolbar>
+      </AppBar>
+      
     </div>
   );
 }
