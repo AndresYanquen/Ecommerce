d957681b5e66f6cd51398c9d0d2a9d10483d7371
Andres Yanquen <andres.yanquen@hotmail.com>
2021-08-30 19:16:10 -0500

    front terminado con validación de datos de api

--- src/components/CompletedText.jsx
+++ src/components/CompletedText.jsx
@@ -2,36 +2,28 @@ import React from "react";
 
 const CompletedText = (percentage) => {
   let texts = {
-    textH3: "Has completado tus tareas",
-    textP1: "Felicitaciones has llegado al 100%",
-    textP2: "",
+    textCompletedH3: "Has completado tus tareas",
+    textCompletedP1: "Felicitaciones has llegado al 100%",
+    textCompletedP2: "",
   };
   let texts2 = {
-    textH3: "Tienes tareas pendientes",
-    textP1: "Te damos algunos tips para que tu tienda sea perfecta.",
-    textP2: "Completa el 100% de toda la configuración, es muy fácil",
-  };
-  const calcular = (percentage) => {
-    console.log(percentage);
-    if (parseInt(toString(percentage)) === 100) {
-      return true;
-    } else {
-      return false;
-    }
+    textPendingH3: "Tienes tareas pendientes",
+    textPendingP1: "Te damos algunos tips para que tu tienda sea perfecta.",
+    textPendingP2: "Completa el 100% de toda la configuración, es muy fácil",
   };
   return (
     <div>
-      {calcular(percentage) ? (
+      {percentage.percentage === 100 ? (
         <>
-          <h3>{texts2.textH3} </h3>
-          <p>{texts2.textP1}</p>
-          <p>{texts2.textP2}</p>
+          <h3>{texts.textCompletedH3} </h3>
+          <p>{texts.textCompletedP1}</p>
+          <p>{texts.textCompletedP2}</p>
         </>
       ) : (
         <>
-          <h3>{texts.textH3} </h3>
-          <p>{texts.textP1}</p>
-          <p>{texts.textP2}</p>
+          <h3>{texts2.textPendingH3} </h3>
+          <p>{texts2.textPendingP1}</p>
+          <p>{texts2.textPendingP2}</p>
         </>
       )}
     </div>
